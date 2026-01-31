72ffce13833f6e030e996beb5c9d4980efb1baf5
Juan Bermudez <eng.juanb@gmail.com>
2026-01-31 16:47:33 -0500

    ad

---
+++ Quantum_DataForce/core/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+
+app_name = 'core'
+
+urlpatterns = [
+    path('', views.inicio, name='inicio'),
+    path('uso-ia/', views.uso_ia, name='uso_ia'),
+    path('chatbot/', views.chatbot_page, name='chatbot_page'),
+    path("chatbot/", views.chatbot_page, name="chatbot"),
+    path("api/chat/", views.chat_api, name="chat_api"),
+    path("energia/", views.consumo_energia, name="consumo_energia"),
+]
